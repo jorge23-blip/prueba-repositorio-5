fb6193de1bf6209f6189ed0367c77940bf8871d2
unknown <jorgepaiva2001@gmail.com>
2025-12-26 00:15:12 -0400

    commit proyecto php

---
+++ .php
@@ -0,0 +1,49 @@
+<? //este es un comentario de una sola linea.
+
+#este es un comentario de una sola linea con almohadilla
+
+/* este es 
+un 
+comentario 
+de 
+multiples lineas o de 
+bloque*/
+
+//variable de texto (string)
+$nombre = "Jorge";
+
+//constante
+define("NOMBRE_USUARIO","jorge paiva");
+
+//numero entero (integer)
+$edad = 30;
+
+//numero de punto flotante
+$precio = 11.67; 
+
+//booleano
+$eszurdo = false;
+$esfacha = true;
+
+//NULL
+$variablenula = null;
+
+//array (arreglo o coleccion de datos)
+$paises = array("venezuela","peru","china");
+
+//resource (recurso) se obtiene de funciones, por ejemplo: $conexion = mysqli_connect(...)
+
+//object (objeto... se crea instanciando una clase)
+//class Persona {}
+// $persona = new Persona();
+
+
+
+
+//sitio web oficial de php
+$url = "https://www.otra-pagina.com";
+$texto_enlace = "Ir a Otra Página";
+
+ 
+
+ ?>
